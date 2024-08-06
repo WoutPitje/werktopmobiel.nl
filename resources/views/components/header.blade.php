3135a98456e897aa45790c64f72d484931124fde
WoutPitje <woutp66@gmail.com>
2024-08-06 08:30:10 +0200

    init

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,6 @@
+<header class="header">
+    <img class="header-logo" src="">
+    <button class="btn">
+        Fix uw website!
+    </button>
+</header>
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        <title>Werkt op mobiel.nl</title>
+    </head>
+    <body>
+        <x-header></x-header>
+        @yield('content')
+    </body>
+</html>
